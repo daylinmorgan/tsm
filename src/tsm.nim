eeb6bca377d2b0ca35c77f2e9cff7b8f82fe690f
Daylin Morgan <me@dayl.in>
2024-11-08 09:44:46 -0600

    chore: upgrade hwylterm

--- src/tsm.nim
+++ src/tsm.nim
@@ -13,43 +13,43 @@ proc tsm(open: bool = false) =
     tmux.attach project.name
 
 
-when isMainModule:
-  import std/parseopt
-  import hwylterm, hwylterm/cli
+# when isMainModule:
+  # import std/parseopt
+import hwylterm, hwylterm/hwylcli
 
-  const
-    tsmVersion {.strdefine.} =
-      staticExec "git describe --tags --always HEAD --match 'v*'"
-    usage =
-      """tmux session manager
+const
+  tsmVersion {.strdefine.} =
+    staticExec "git describe --tags --always HEAD --match 'v*'"
+  usage =
+    """tmux session manager
 
-[bold]tsm[/] [[[faint]-h|-v|-o[/]]"""
-    flags = [
-        ("h","help","show this help"),
-        ("v","version", "print version"),
-        ("o","open", "only search open sessions")
-    ]
-  proc help() = echo newHwylCli(usage, flags = flags)
-  var open: bool
-  var p = initOptParser(
-    shortNoVal = {'h', 'v', 'o'},
-    longNoVal = @["open"]
-  )
-  for kind, key, val in p.getOpt():
-    case kind:
-    of cmdEnd:
-      break
-    of cmdArgument:
-      echo bb"[red]Error[/]: unexpected positional argument ", bbfmt"[bold]{key}[/]"
-    of cmdShortOption, cmdLongOption:
-      case key:
-      of "help", "h":
-        help(); quit 0
-      of "version", "v":
-        echo "tsm: " & tsmVersion; quit 0
-      of "open", "o":
-        open = true
-      else:
-        echo bbfmt"[red]Error[/]: unknown key value pair", bbfmt"[b]key[/]: {key}, [b]value[/]: {val}"
+ [bold]tsm[/] [[[faint]-h|-v|-o[/]]"""
+  flags = [
+      ("h","help","show this help"),
+      ("v","version", "print version"),
+      ("o","open", "only search open sessions")
+  ]
+proc help() = echo newHwylCliHelp(usage = usage, flags = flags)
+var open: bool
+var p = initOptParser(
+  shortNoVal = {'h', 'v', 'o'},
+  longNoVal = @["open"]
+)
+for kind, key, val in p.getOpt():
+  case kind:
+  of cmdError: assert false
+  of cmdEnd: assert false
+  of cmdArgument:
+    echo bb"[red]Error[/]: unexpected positional argument ", bbfmt"[bold]{key}[/]"
+  of cmdShortOption, cmdLongOption:
+    case key:
+    of "help", "h":
+      help(); quit 0
+    of "version", "v":
+      echo "tsm: " & tsmVersion; quit 0
+    of "open", "o":
+      open = true
+    else:
+      echo bbfmt"[red]Error[/]: unknown key value pair", bbfmt"[b]key[/]: {key}, [b]value[/]: {val}"
 
-  tsm(open)
+tsm(open)
