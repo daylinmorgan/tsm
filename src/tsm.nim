4b53d3e050e4bb444ef28bdc8c2e11b939d8dfb2
Daylin Morgan <me@dayl.in>
2024-11-12 15:18:20 -0600

    update to hwylcli

--- src/tsm.nim
+++ src/tsm.nim
@@ -1,6 +1,6 @@
 import ./[selector, project, tmuxutils]
+import hwylterm, hwylterm/hwylcli
 
-# TODO: add option to only opened configured sessions
 proc tsm(open: bool = false) =
   let
     projects = findProjects(open)
@@ -12,44 +12,11 @@ proc tsm(open: bool = false) =
   else:
     tmux.attach project.name
 
+hwylCli:
+  name "tsm"
+  V tsmVersion
+  flags:
+    open "only search open sessions"
+  run:
+    tsm(open)
 
-# when isMainModule:
-  # import std/parseopt
-import hwylterm, hwylterm/hwylcli
-
-const
-  tsmVersion {.strdefine.} =
-    staticExec "git describe --tags --always HEAD --match 'v*'"
-  usage =
-    """tmux session manager
-
- [bold]tsm[/] [[[faint]-h|-v|-o[/]]"""
-  flags = [
-      ("h","help","show this help"),
-      ("v","version", "print version"),
-      ("o","open", "only search open sessions")
-  ]
-proc help() = echo newHwylCliHelp(usage = usage, flags = flags)
-var open: bool
-var p = initOptParser(
-  shortNoVal = {'h', 'v', 'o'},
-  longNoVal = @["open"]
-)
-for kind, key, val in p.getOpt():
-  case kind:
-  of cmdError: assert false
-  of cmdEnd: assert false
-  of cmdArgument:
-    echo bb"[red]Error[/]: unexpected positional argument ", bbfmt"[bold]{key}[/]"
-  of cmdShortOption, cmdLongOption:
-    case key:
-    of "help", "h":
-      help(); quit 0
-    of "version", "v":
-      echo "tsm: " & tsmVersion; quit 0
-    of "open", "o":
-      open = true
-    else:
-      echo bbfmt"[red]Error[/]: unknown key value pair", bbfmt"[b]key[/]: {key}, [b]value[/]: {val}"
-
-tsm(open)
