021167ecaef0abfa43c2b285b02f6f323e692b9e
Daylin Morgan <me@dayl.in>
2025-11-04 13:55:29 -0500

    update functions

--- nix/flake.nix
+++ nix/flake.nix
@@ -13,46 +13,51 @@
     }:
     let
       inherit (nixpkgs.lib) genAttrs;
-      supportedSystems = [
+      systems = [
         "x86_64-linux"
         "x86_64-darwin"
         "aarch64-linux"
         "aarch64-darwin"
       ];
-      forAllSystems =
-        f:
-        genAttrs supportedSystems (
-          system:
-          f (import nixpkgs {
+      forSystem =
+        f: system:
+        f system (
+          import nixpkgs {
             inherit system;
-            overlays = [nim2nix.overlays.default];
+            overlays = [ nim2nix.overlays.default ];
           }
-        ));
+        );
+      forAllSystems = f: genAttrs systems (forSystem f);
+
       rev = toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown");
     in
     {
-      devShells = forAllSystems (pkgs: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            nim
-            nim-atlas # from overlay
-          ];
-        shellHook= ''
-          echo -e "\n\nget started by running 'atlas rep' if needed"
-        '';
-        };
-      });
-      packages = forAllSystems (pkgs: {
-        tsm = pkgs.buildAtlasPackage rec {
-          pname = "tsm";
-          version = "2025.1004-unstable-${rev}"  ;
-          src = ../.;
-          atlasDepsHash = "sha256-S2Z3kALdNcZmS09aC/kwKSaXsgBTnAM0WXSyjbzfSfs=";
-          nimFlags = [
-            "-d:TsmVersion=v${version}"
-          ];
-        };
-        default = self.packages.${pkgs.system}.tsm;
-      });
+      devShells = forAllSystems (
+        _: pkgs: {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              nim
+              nim-atlas # from overlay
+            ];
+            shellHook = ''
+              echo -e "\n\nget started by running 'atlas rep' if needed"
+            '';
+          };
+        }
+      );
+      packages = forAllSystems (
+        system: pkgs: {
+          tsm = pkgs.buildAtlasPackage rec {
+            pname = "tsm";
+            version = "2025.1004-unstable-${rev}";
+            src = ../.;
+            atlasDepsHash = "sha256-S2Z3kALdNcZmS09aC/kwKSaXsgBTnAM0WXSyjbzfSfs=";
+            nimFlags = [
+              "-d:TsmVersion=v${version}"
+            ];
+          };
+          default = self.packages.${system}.tsm;
+        }
+      );
     };
 }
